e001c896e03f2a58c3022e55ea3522eeff999122
Blake Gideon <blake@gideon.email>
2020-12-22 11:59:46 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,9 +4,9 @@ let mySprite = sprites.create(img`
     . . . . . . . . . b c . . . . . 
     . . . . . . b b b b b b . . . . 
     . . . . . b b 5 5 5 5 5 b . . . 
-    . . . . b b 5 d 1 2 5 5 d 2 . . 
-    . . . . b 5 5 1 2 2 5 d 4 c . . 
-    . . . . b 5 5 d 2 b d d 4 4 . . 
+    . . . . b b 5 d 1 7 5 5 d 7 . . 
+    . . . . b 5 5 1 7 7 5 d 4 c . . 
+    . . . . b 5 5 d 7 b d d 4 4 . . 
     b d d d b b d 5 5 5 4 4 4 4 4 b 
     b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
     b d c 5 5 5 5 d 5 5 5 5 5 b . . 
